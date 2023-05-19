db6f7962c799cdbb9961150f46a8d8b0c543be2c
joohyun7u <joohyun7u@gmail.com>
2023-05-19 15:44:39 +0900

    basic 작업

---
+++ cjh/run_ariel.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#SBATCH --job-name in110-4
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-gpu=6
+#SBATCH --mem-per-gpu=24G
+#SBATCH --time 1-0
+#SBATCH --partition batch_ugrad
+#SBATCH -w ariel-g4
+#SBATCH -o logs/slurm-%A-%x.out
+
+source /data/joohyun7u/cjh/sh/setup.sh
+
+current_time=$(date "+%Y%m%d-%D:%M:%S")
+echo $current_time
+py_dir=../MLP/resnet50_image2.py
+dataset_dir=/home/joohyun7u/dataset/ff
+
+#lrs=(4e-2, 8e-2, 4e-3, 8e-3, 4e-4, 4e-5)
+#lr="{lrs[SLURM_ARRAY_TASK_ID]}"
+
+#tar -xcvf /data/datasets/ImageNet.tar -C /local_datasets/
+
+python -u $py_dir
+
+# python train.py \
+#     --cfg-options \
+#         optimizer.lr=$lr
+
+echo 'done'
+exit 0
---
+++ cjh/unzip.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+
+
+echo 'done'
+exit 0
