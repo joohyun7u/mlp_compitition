bbfdbd0be87bafb4077b1ea8c57bb5d3669ded98
joohyun7u <joohyun7u@gmail.com>
2023-05-26 21:49:40 +0900

    test.zip 변경 지원

--- unzip.sh
+++ unzip.sh
@@ -10,16 +10,18 @@
 #SBATCH -o logs/slurm-%A-%x.out
 base_dir=/local_datasets/MLinP
 
-fold=$base_dir'/test/scan'
+fold=$base_dir'/test/scan/'
 if [ -d $fold ]; then
     echo $fold' exist'
 else
     echo $fold' not exist'
     mkdir -p $fold
 fi
+
 
 file=$fold'/Wireframe57.00108.02.tif'
 target_zip='test_scan.zip'
+echo $file
 if [ -f $file ]; then
     echo $target_zip' 이미 압축 해제 되었음'
 else
@@ -28,10 +30,10 @@ else
     echo $target_zip' 압축 해제중'
     unzip $target_zip -d $fold
 fi
+stat $fold
 
 
-
-fold=$base_dir'/train/scan'
+fold=$base_dir'/train/scan/'
 if [ -d $fold ]; then
     echo $fold' exist'
 else
@@ -39,19 +41,21 @@ else
     mkdir -p $fold
 fi
 
-file=$fold'BookOfMaking.00002.02.tif'
+file=$fold'Wireframe57.00116.01.tif'
 target_zip='train_scan.zip'
+echo $file
 if [ -f $file ]; then
     echo $target_zip' 이미 압축 해제 되었음'
 else
+    echo heheh
     rm -rf $fold
     mkdir -p $fold
     echo $target_zip' 압축 해제중'
     unzip $target_zip -d $fold
 fi
+stat $fold
 
-
-fold=$base_dir'/train/clean'
+fold=$base_dir'/train/clean/'
 if [ -d $fold ]; then
     echo $fold' exist'
 else
@@ -59,7 +63,7 @@ else
     mkdir -p $fold
 fi
 
-file=$fold'BookOfMaking.00002.02.tif'
+file=$fold'Wireframe57.00116.01.tif'
 target_zip='train_clean.zip'
 if [ -f $file ]; then
     echo $target_zip' 이미 압축 해제 되었음'
@@ -69,3 +73,6 @@ else
     echo $target_zip' 압축 해제중'
     unzip $target_zip -d $fold
 fi
+stat $fold
+
+du $base_dir
